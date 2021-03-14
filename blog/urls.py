de1fdba74b1c0e46908d14678788a69a4298635f
Bharat Paliwal <18uec169@lnmiit.ac.in>
2021-03-15 01:13:44 +0530

    CRUD continue..

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from .views import PostListView,PostDetailView,PostCreateView
 urlpatterns = [
   path("",PostListView.as_view(),name="blog_home"),
   path("post/<int:pk>/",PostDetailView.as_view(),name="Post_details"),
-  path("post/new/",PostListView.as_view(),name="post-created"),
+  path("post/new/",PostCreateView.as_view(),name="post-create"),
   path("about/",views.about,name="blog_about"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.views.generic import ListView,DetailView,CreateView
 # data from database
 
@@ -42,7 +44,11 @@ class PostDetailView(DetailView):
   model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(  LoginRequiredMixin , CreateView):
   model = Post
   fields = ['title', 'content']
+
+  def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
