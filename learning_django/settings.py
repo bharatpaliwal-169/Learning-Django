8208002c997c91f1a2faa4a6e4196f74fe047eee
Bharat Paliwal <18uec169@lnmiit.ac.in>
2021-03-16 00:52:08 +0530

    Completed applicaion now deploy

--- learning_django/settings.py
+++ learning_django/settings.py
@@ -129,3 +129,15 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog_home'
 LOGIN_URL = 'login'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'development.id.2301@gmail.com'
+EMAIL_HOST_PASSWORD = 'development@2301'
+
+# ise nahi ho raha
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
